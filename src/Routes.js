28b06ad33a47920db8fa8ae9e995d48a877b0c82
madhavigiduthuri <madhavi.giduthuri@gmail.com>
2023-06-20 15:08:56 +0530

    designed few screens

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,19 @@
 import LoginPage from "./pages/LoginPage.jsx";
-import SignupPage from "./pages/SignupPage.jsx"; 
+import SignupPage from "./pages/SignupPage.jsx";
 import ActivationPage from "../src/pages/ActivationPage";
+import HomePage from "../src/pages/HomePage";
+import ProductsPage from "../src/pages/ProductsPage";
+import BestSellingPage from "../src/pages/BestSellingPage";
+import EventsPage from "../src/pages/EventsPage";
+import FAQPage from "../src/pages/FAQPage";
 
 export {
-    LoginPage,
-    SignupPage,
-    ActivationPage,
+  LoginPage,
+  SignupPage,
+  ActivationPage,
+  HomePage,
+  ProductsPage,
+  BestSellingPage,
+  EventsPage,
+  FAQPage,
 };
