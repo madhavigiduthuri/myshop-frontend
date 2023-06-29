f7c76e82d3cf63a34250be4a5c431c9f9e1fcb06
madhavigiduthuri <madhavi.giduthuri@gmail.com>
2023-06-29 13:03:56 +0530

    best seller signUp and signIn creation done

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -7,7 +7,7 @@ const styles = {
   productDiscountPrice: "font-bold text-[18px] text-[#333] font-Roboto",
   price: "font-[500] text-[16px] text-[#d55b45] pl-3 mt-[-4px] line-through",
   shop_name: "pt-3 text-[15px] text-blue-400 pb-3",
-  active_indicator: "absolute bottom-[-27%] left-0 h-[3px] w-full bg-[crimson]",
+  active_indicator: "absolute bottom-[-27%] left-0 h-[3px] w-full bg-[#32a850]",
   button:
     "w-[150px] bg-[#32a850] h-[50px] my-3 flex items-center justify-center rounded-xl cursor-pointer",
   cart_button:
